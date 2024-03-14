09660f1cb9ba1927387fd9f3d3725d4ead49efe2
SelimYuksel <yukselselim26@gmail.com>
2024-03-14 09:59:04 +0300

    Add project files.

---
+++ FrmOgrenciNotlar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SchoolProject
+{
+    public partial class FrmOgrenciNotlar : Form
+    {
+        public FrmOgrenciNotlar()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-5CQC2MM;Initial Catalog=OkulProje;Integrated Security=True");
+        public string numara;
+        private void FrmOgrenciNotlar_Load(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("Select * From TBLNOTLAR Where OGRID=@P1", connection);
+            sqlCommand.Parameters.AddWithValue("@P1",numara);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
