4cb57582e3622d5dc587f5a5e501281663a28bb7
SelimYuksel <yukselselim26@gmail.com>
2024-03-28 11:14:13 +0300

    Öğretmen formu eklendi

--- FrmOgrenciNotlar.cs
+++ FrmOgrenciNotlar.cs
@@ -22,12 +22,32 @@ namespace SchoolProject
         public string numara;
         private void FrmOgrenciNotlar_Load(object sender, EventArgs e)
         {
-            SqlCommand sqlCommand = new SqlCommand("Select * From TBLNOTLAR Where OGRID=@P1", connection);
-            sqlCommand.Parameters.AddWithValue("@P1",numara);
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            SqlCommand sqlCommand1 = new SqlCommand("SELECT DERSAD, SINAV1, SINAV2, PROJE, ORTALAMA, DURUM FROM TBLNOTLAR\r\nINNER JOIN TBLDERSLER ON TBLNOTLAR.DERSID = TBLDERSLER.DERSID WHERE OGRID=@P1", connection);
+            sqlCommand1.Parameters.AddWithValue("@P1",numara);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand1);
             DataTable dataTable = new DataTable();
             sqlDataAdapter.Fill(dataTable);
             dataGridView1.DataSource = dataTable;
+
+            SqlCommand sqlCommand2 = new SqlCommand("SELECT OGRAD, OGRSOYAD FROM TBLOGRENCILER WHERE OGRID=@P2", connection);
+            sqlCommand2.Parameters.AddWithValue("@P2", numara);
+
+            connection.Open();
+
+            SqlDataReader dataReader = sqlCommand2.ExecuteReader();
+            if (dataReader.Read())
+            {
+                string ad = dataReader["OGRAD"].ToString();
+                string soyad = dataReader["OGRSOYAD"].ToString();
+                this.Text = ad + " " + soyad;
+            }
+            else
+            {
+                MessageBox.Show("Kayıt bulunamadı");
+            }
+            connection.Close();
+
+
         }
     }
 }
